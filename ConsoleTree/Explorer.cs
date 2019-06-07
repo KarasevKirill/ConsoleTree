420eaff89f0bcfd3a2a1986ec863e05bf2d1b1b1
Kirill Karasev <karasev.kirill.a@gmail.com>
2019-06-07 21:36:59 +1000

    Final

---
+++ ConsoleTree/Explorer.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+
+namespace DNSTest
+{
+    class Explorer
+    {
+        public bool UseDepth { get; set; }
+
+        public int Depth { get; set; }
+
+        public bool UseSize { get; set; }
+
+        public bool UseReadable { get; set; }
+
+        public void DisplayTree(string folderPath)
+        {
+            DisplayTree(folderPath, "", 1);
+        }
+
+        private void DisplayTree(string folderPath, string line, int currentDepth)
+        {
+            if (UseDepth && currentDepth > Depth)
+                return;
+
+            var currentFolder = new DirectoryInfo(folderPath);
+
+            var files = currentFolder.GetFiles();
+            var folders = currentFolder.GetDirectories();
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                var file = files[i];
+
+                if (i == files.Length - 1 && folders.Length == 0)
+                    Console.WriteLine($"{line}└──{file.Name}{GetFileSize(file)}");
+                else
+                    Console.WriteLine($"{line}├──{file.Name}{GetFileSize(file)}");
+            }
+
+            currentDepth++;
+
+            for (int i = 0; i < folders.Length; i++)
+            {
+                var folder = folders[i];
+
+                if (i == folders.Length - 1)
+                {
+                    Console.WriteLine($"{line}└──{folder.Name}");
+                    DisplayTree(folder.FullName, $"{line}    ", currentDepth);
+                }
+                else
+                {
+                    Console.WriteLine($"{line}├──{folder.Name}");
+                    DisplayTree(folder.FullName, $"{line}│   ", currentDepth);
+                }
+            }
+        }
+
+        private bool IsFolder(FileSystemInfo item)
+        {
+            return (item.Attributes & FileAttributes.Directory) == FileAttributes.Directory;
+        }
+
+        private string GetFileSize(FileInfo file)
+        {
+            if (!UseSize)
+                return "";
+
+            if (UseReadable)
+                return $" ({GetFileSize(file.Length)})";
+            else
+                return $" ({file.Length})";
+        }
+
+        private string GetFileSize(long length)
+        {
+            if (!UseReadable)
+                return length.ToString();
+
+            var suf = new string[] { " B", " KB", " MB", " GB", " TB" };
+
+            if (length == 0)
+                return "empty";
+
+            var bytes = Math.Abs(length);
+
+            var place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+
+            var num = Math.Round(bytes / Math.Pow(1024, place), 1);
+
+            return $"{(Math.Sign(length) * num)}{suf[place]}";
+        }
+    }
+}
--- ConsoleTree/Program.cs
+++ ConsoleTree/Program.cs
@@ -1,15 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleTree
+namespace DNSTest
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var explorer = new Explorer();
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] == "-d" || args[i] == "--depth")
+                {
+                    explorer.UseDepth = true;
+
+                    try
+                    {
+                        int depth = Convert.ToInt32(args[i + 1]);
+
+                        explorer.Depth = depth;
+
+                    }
+                    catch
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("При использовании параметров -d или --depth необходимо указать глубину вложенности");
+                        Console.ResetColor();
+                        return;
+                    }
+                }
+
+                if (args[i] == "-h" || args[i] == "--human-readable")
+                    explorer.UseReadable = true;
+
+                if (args[i] == "-s" || args[i] == "--size")
+                    explorer.UseSize = true;
+
+                if (args[i] == "--help" || args[i] == "-?")
+                {
+                    ShowHelp();
+                    return;
+                }
+            }
+
+            explorer.DisplayTree(Environment.CurrentDirectory);
+        }
+
+        private static void ShowHelp()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Консольное приложение, которые выводит содержимое текущего и вложенных каталогов в виде дерева");
+            Console.WriteLine("Дополнительные агрументы при запуске приложения:");
+            Console.WriteLine("-d или --depth - задать глубину вложенности. Сама глубина задается целым числом, указываемым после аргумента");
+            Console.WriteLine("-s или --size - отображать размер файлов");
+            Console.WriteLine("-h или --human-readable - отображать размер файлов в удобном для восприятия виде");
+            Console.WriteLine("--help или -? - справка");
+            Console.ResetColor();
         }
     }
 }
